b54abb8a6fe682c9c247cf958f600c20b2501e44
Tu Nombre <tu-correo@gmail.com>
2026-02-07 00:57:48 -0600

    Conexión oficial a Firebase y diseño de tienda

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaCz52lh8uU8y-oS4muCeIPVroPdLs2ws",
+  authDomain: "vada-gt.firebaseapp.com",
+  projectId: "vada-gt",
+  storageBucket: "vada-gt.firebasestorage.app",
+  messagingSenderId: "412863431800",
+  appId: "1:412863431800:web:34af7878daba6f70ca198d"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const db = getFirestore(app);
